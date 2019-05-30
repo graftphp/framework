ae7c78a0214f9fc793a76f2b8fef51531cb4aea4
Roger Hughes <me@rogerhughes.pro>
2019-05-30 10:36:44 +0100

    parse embed tags after compiling the template

--- src/View.php
+++ src/View.php
@@ -22,18 +22,6 @@ class View
         include $path;
         $view_contents = ob_get_clean();
 
-        // check for and insert any embed content
-        preg_match_all("/\{embed:(.*)\}/", $view_contents, $embed_tags);
-        if (count($embed_tags[0]) > 0) {
-            foreach ($embed_tags[0] as $index => $embed) {
-                $embed_path = $thisViewPath . str_replace('.', '/', $embed_tags[1][$index]) . '.php';
-                ob_start();
-                include($embed_path);
-                $embed_contents = ob_get_clean();
-                $view_contents = str_replace($embed, $embed_contents, $view_contents);
-            }
-        }
-
         // check for a template tag, we will only use the first one
         preg_match_all("/\{template:(.*)\}/", $view_contents, $template_tag);
         if (count($template_tag[0]) > 0) {
@@ -43,7 +31,7 @@ class View
             $template_contents = ob_get_clean();
 
             // replace template regions with content from the child view
-            preg_match_all("/\{(.*)\}/", $template_contents, $regions);
+            preg_match_all("/\{((?!embed).*)\}/", $template_contents, $regions);
             foreach ($regions[1] as $region) {
                 $start = '{' . $region . '}';
                 $end = "{/$region}";
@@ -51,12 +39,24 @@ class View
                 $endpos = strpos($view_contents, $end);
                 $chunk = '';
                 if ($startpos && $endpos) {
-                    $startpos = $startpos+strlen($start);
+                    $startpos = $startpos + strlen($start);
                     $chunk = substr($view_contents, $startpos, $endpos - $startpos);
                 }
                 $template_contents = str_replace($start, $chunk, $template_contents);
             }
             $view_contents = $template_contents;
+        }
+
+        // check for and insert any embed content
+        preg_match_all("/\{embed:(.*)\}/", $view_contents, $embed_tags);
+        if (count($embed_tags[0]) > 0) {
+            foreach ($embed_tags[0] as $index => $embed) {
+                $embed_path = $thisViewPath . str_replace('.', '/', $embed_tags[1][$index]) . '.php';
+                ob_start();
+                include $embed_path;
+                $embed_contents = ob_get_clean();
+                $view_contents = str_replace($embed, $embed_contents, $view_contents);
+            }
         }
 
         echo $view_contents;
